154ed2721d97ae44038eb27b0f62e916a09e770b
Igor Ramos <isramos@gmail.com>
2021-03-20 00:23:04 -0500

    optimize config

--- app/server.js
+++ app/server.js
@@ -1,4 +1,7 @@
-require('dotenv').config()
 
+if(process.env.WORDPRESS_API_BASE === undefined){
+    console.log('Error: Could not log dotenv config file')
+    process.exit(1)
+}
 console.log('Hello world! WORDPRESS_API_BASE: ', process.env.WORDPRESS_API_BASE)
 
