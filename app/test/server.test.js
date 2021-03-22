90faea536b7781a15cd1899bb06247529a37ba7d
Igor Ramos <isramos@gmail.com>
2021-03-21 20:10:28 -0500

    Update server.test.js

--- app/test/server.test.js
+++ app/test/server.test.js
@@ -1,11 +1,13 @@
 const srv = require('../server')
 
+/*
+    This file is to help testing some functions
 
+*/
 // unpack test data
 const {TEST_ORDER:testOrder, TEST_USER: testUser, TEST_ZIP:testPostalCode } = process.env;
 
 ;(async() => {
-
     srv.getOrder(testUser, testOrder, testPostalCode)
-
+    srv.addNote(testOrder, "This is a test Note")
 })()
